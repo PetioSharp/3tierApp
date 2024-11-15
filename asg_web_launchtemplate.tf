7ff563820afd9a57079b201b843349a46ab2cf8c
savovpe <peter.savov@arhs-dev-bulgaria.com>
2024-11-15 15:42:16 +0200

    working on three tier app

--- asg_web_launchtemplate.tf
+++ asg_web_launchtemplate.tf
@@ -2,7 +2,7 @@
 resource "aws_autoscaling_group" "three-tier-asg" {
   name                 = "three-tier-web-asg"
   launch_configuration =  aws_launch_configuration.three-tier-web-lconfig.id
-  vpc_zone_identifier  =  [aws_subnet.three-tier-pub-sub-1, aws_subnet.three-tier-pub-sub-2]
+  vpc_zone_identifier  =  [aws_subnet.three-tier-pub-sub-1.id, aws_subnet.three-tier-pub-sub-2.id]
   min_size             = 2
   max_size             = 3
   desired_capacity     = 2    
